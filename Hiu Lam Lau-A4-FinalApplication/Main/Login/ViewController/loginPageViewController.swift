befa34cfc2b61f642b17772a1befc64d34e8ab29
Hiu Lam Lau <hlau0019@student.monash.edu>
2023-04-10 22:38:10 +1000

    login segue successful connected with home page and home page UI design creation

--- Hiu Lam Lau-A4-FinalApplication/Main/Login/ViewController/loginPageViewController.swift
+++ Hiu Lam Lau-A4-FinalApplication/Main/Login/ViewController/loginPageViewController.swift
@@ -33,7 +33,7 @@ class loginPageViewController: UIViewController {
             } else {
                 // Login successful, perform any action or segue to the next screen
                 print("Logged in successfully")
-                // strongSelf.performSegue(withIdentifier: "yourSegueIdentifier", sender: nil)
+                strongSelf.performSegue(withIdentifier: "homePage", sender: nil)
             }
         }
 
@@ -56,14 +56,14 @@ class loginPageViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+    
 
 }
