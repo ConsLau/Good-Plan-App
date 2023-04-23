7bef40296fa4edd8477fb2f25146bd1cfd65eb32
Hiu Lam Lau <hlau0019@student.monash.edu>
2023-04-24 03:03:14 +1000

    CreateTaskViewController creation

---
+++ Hiu Lam Lau-A4-FinalApplication/ToDoList/CreateTaskViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateTaskViewController.swift
+//  Hiu Lam Lau-A4-FinalApplication
+//
+//  Created by Cons Lau on 24/4/2023.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
